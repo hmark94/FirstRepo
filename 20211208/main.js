498fba71b956f71445db386945c734cee58a4260
Hatala Mark <hatala.mark@gmail.com>
2021-12-09 19:40:36 +0100

    2021_12_09

--- 20211208/main.js
+++ 20211208/main.js
@@ -1,3 +1,6 @@
+// src = "https://jsbin.com/sixazew/1/edit?js"
+
+
 let phoneNumbers = [3630792765, 3630369549, 3620797249, 3670129465, 3630735165, 3630799685, 3630294765, 3630739572, 3630792388, 3630792947, 3630798965, 3630799284, 3630793325, 3630799685, 3630194265,];
 let names = ['Bandobras Underhill', 'Robin Gardner', 'Isembold Whitfoot', 'Donnamira Galbassi', 'Bungo Baggins', 'Will Sandyman', 'Isengrim Proudfoot', 'Frodo Baggins', 'Amaranth Gaukrogers', 'Trahand Bunce', 'Will Sandyman', 'Halfred Goodchild', 'Bilbo Baggins', 'Will Sandyman', 'Belba Sandheaver'];
 
@@ -86,11 +89,68 @@ function find(myPhoneBook, value) {
 
 function filterByName(myPhoneBook, nev) {
     let tomb = [];
-    for (let i = 0; i < myPhoneBook.lenth; i++){
+    for (let i = 0; i < myPhoneBook.length; i++) {
         if (myPhoneBook[i].name == nev) {
-            tomb.push(i);
-            return tomb;
+            tomb.push(myPhoneBook[i]);
         }
-    } return null;
+    }
+    return tomb;
 }
-console.log(filterByName(myPhoneBook, 'Mark'));
+
+//console.log(filterByName(myPhoneBook, 'Mark'));
+
+// Callback fgv-el
+
+function filterByNameCallback(myPhoneBook, predicate) {
+    let tomb = [];
+    for (let i = 0; i < myPhoneBook.length; i++) {
+        if (predicate(myPhoneBook[i].name)) {
+            tomb.push(myPhoneBook[i]);
+        }
+    } return tomb;
+}
+//console.log(
+    filterByNameCallback(myPhoneBook, function (name) { return name === 'Mark' }),
+    filterByNameCallback(myPhoneBook, function (tomb) { return tomb.name === 'Mark' }),
+    filterByNameCallback(myPhoneBook, function (tomb) { return tomb.mobile === 36701234567 })
+//);
+
+//telefonszám alapján töröl
+
+function remove(myPhoneBook, mobile) {
+    let index = indexByMobile(myPhoneBook, mobile);
+    if (index > -1) {
+        return myPhoneBook.splice(index, 1);
+    }
+    return null;
+}
+//console.log(remove(myPhoneBook, 36701234567));
+
+function addField(myPhoneBook, mobile, field, value) {
+    let entry = findByMobile(myPhoneBook, mobile);
+    if (entry != null) {
+        entry[field] = value;
+        return entry;
+    }
+    return null;
+}
+// console.log(addField(myPhoneBook, 36701234567, 'email', 'valami@info.com'));
+
+function modify(myPhoneBook, mobile, field, value) {
+    let entry = findByMobile(myPhoneBook, mobile);
+    if (entry != null && entry[field] !== undefined) {
+        entry[field] = value;
+        return entry;
+    }
+    if (field === 'mobile') {
+        return null;
+    }
+}
+// console.log(modify(myPhoneBook, 36701234567, 'email', 'valami2@info.com'));
+
+function formatPhoneNumber(mobile) {
+    mobile = String(mobile);
+    return '+' + mobile.slice(0, 2) + ' ' + mobile.slice(2, 4) + ' ' + mobile.slice(4, 8) + ' ' + mobile.slice(8);
+}
+
+// console.log(formatPhoneNumber(36701234567));
